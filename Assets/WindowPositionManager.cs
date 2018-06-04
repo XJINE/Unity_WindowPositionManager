983f9f59103057f3d78a1f28f201ed0332cbc9c2
XJINE <xjine.andorelseif@gmail.com>
2018-06-04 20:33:02 +0900

    First commit.

---
+++ Assets/WindowPositionManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class WindowPositionManager : SingletonMonoBehaviour<WindowPositionManager>
+{
+    #region DllImport
+
+    // # Reference
+    // https://msdn.microsoft.com/ja-jp/library/cc411206.aspx
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll", EntryPoint = "SetWindowPos")]
+    private static extern bool SetWindowPos(IntPtr hWnd,
+                                            int hWndInsertAfter,
+                                            int x,
+                                            int y,
+                                            int cx,
+                                            int cy,
+                                            int uFlags);
+
+    #endregion DllImport
+
+    #region Field
+
+    public static readonly string CommandScreenPositionX = "-screen-position-x";
+    public static readonly string CommandScreenPositionY = "-screen-position-y";
+    public static readonly string CommandTopmost         = "-topmost";
+
+    #endregion Field
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        #if UNITY_EDITOR
+        
+        return;
+
+        #endif
+
+        int  screenPositionX = CommandLineArgs.GetValueAsInt (CommandScreenPositionX, out screenPositionX) ? screenPositionX : 0;
+        int  screenPositionY = CommandLineArgs.GetValueAsInt (CommandScreenPositionY, out screenPositionY) ? screenPositionY : 0;
+        bool topmost         = CommandLineArgs.HasParameter  (CommandTopmost);
+
+        SetWindowPosition(screenPositionX, screenPositionY, topmost);
+    }
+
+    protected virtual void SetWindowPosition(int x, int y, bool topmost)
+    {
+        // NOTE:
+        // Window will be set topmost when the value of "topmost" is -1.
+
+        SetWindowPos(GetActiveWindow(),
+                     topmost ? -1 : 0,
+                     x,
+                     y,
+                     Screen.width,
+                     Screen.height,
+                     0);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# Unity3D_WindowPositionController
-Unity3D Windows Standalone Player に、実行ウィンドウの位置および最前面表示の可否を設定するような、コマンドラインオプションを追加します。
+# Unity_WindowPositionManager
+
+Add some command line arguments to set window position.
+
+## Import to Your Project
+
+If you want to import this repository as submodule in your project.
+
+```
+git submodule add -b submodule https://github.com/XJINE/Unity_WindowPositionManager.git Assets/Packages/WindowPositionManager
+```
+
+### Dependencies
+
+- https://github.com/XJINE/Unity_CommandLineArgs
+- https://github.com/XJINE/Unity_SingletonMonoBehaviour
+
+## Arguments
+
+| Argument             | Description     |
+|:---------------------|:--------------------------------------|
+| *-screen-position-x* | Set window top-left position X in px. |
+| *-screen-position-y* | Set window top-left position Y in px. |
+| *-topmost*           | Set window topmost.                   |
+
+## Limitation
+
+- Window must be popupwindow or windowed mode.
+- Window must be active while loading scene(script).
+    - If you activate the another window while scene load,
+      these settings works wrong.
+
+## Sample
+
+```
+WindowPositionManager.exe -popupwindow -screen-width 640 -screen-height 480 -screen-position-x 200 -screen-position-y 200 -topmost
+```
+
+## Screenshot
+
+![](https://github.com/XJINE/Unity3D_WindowPositionManager/blob/master/screenshot0.gif)
